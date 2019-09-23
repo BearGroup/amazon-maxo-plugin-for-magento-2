cc0f180ce6e187fdab8db2ac6751e87e6dd6e89f
Jonah Ellison <jonah@beargroup.com>
2019-09-23 14:47:07 -0700

    APF-170 empty cart after checkout

--- src/PayV2/Controller/Payment/CompleteCheckout.php
+++ src/PayV2/Controller/Payment/CompleteCheckout.php
@@ -18,6 +18,7 @@ namespace Amazon\PayV2\Controller\Payment;
 use Magento\Framework\App\ObjectManager;
 use Magento\Quote\Api\CartManagementInterface;
 use Amazon\Core\Logger\ExceptionLogger;
+use Magento\Framework\App\PageCache\Version;
 
 /**
  * Class CompleteCheckout
@@ -52,12 +53,25 @@ class CompleteCheckout extends \Magento\Framework\App\Action\Action
     private $exceptionLogger;
 
     /**
+     * @var \Magento\Framework\Stdlib\CookieManagerInterface
+     */
+    private $cookieManager;
+
+    /**
+     * @var \Magento\Framework\Stdlib\Cookie\CookieMetadataFactory
+     */
+    private $cookieMetadataFactory;
+
+
+    /**
      * CompleteCheckout constructor.
      * @param \Magento\Framework\App\Action\Context $context
      * @param \Amazon\PayV2\CustomerData\CheckoutSession $amazonCheckoutSession
      * @param CartManagementInterface $cartManagement
      * @param \Magento\Checkout\Model\Session $checkoutSession
      * @param \Magento\Customer\Model\Session $session
+     * @param \Magento\Framework\Stdlib\CookieManagerInterface $cookieManager
+     * @param \Magento\Framework\Stdlib\Cookie\CookieMetadataFactory $cookieMetadataFactory
      * @param ExceptionLogger|null $exceptionLogger
      */
     public function __construct(
@@ -66,6 +80,8 @@ class CompleteCheckout extends \Magento\Framework\App\Action\Action
         CartManagementInterface $cartManagement,
         \Magento\Checkout\Model\Session $checkoutSession,
         \Magento\Customer\Model\Session $session,
+        \Magento\Framework\Stdlib\CookieManagerInterface $cookieManager,
+        \Magento\Framework\Stdlib\Cookie\CookieMetadataFactory $cookieMetadataFactory,
         ExceptionLogger $exceptionLogger = null
     ) {
         parent::__construct($context);
@@ -74,6 +90,8 @@ class CompleteCheckout extends \Magento\Framework\App\Action\Action
         $this->checkoutSession = $checkoutSession;
         $this->session = $session;
         $this->exceptionLogger = $exceptionLogger ?: ObjectManager::getInstance()->get(ExceptionLogger::class);
+        $this->cookieManager = $cookieManager;
+        $this->cookieMetadataFactory = $cookieMetadataFactory;
     }
 
     /*
@@ -91,6 +109,7 @@ class CompleteCheckout extends \Magento\Framework\App\Action\Action
                 }
                 $this->cartManagement->placeOrder($this->checkoutSession->getQuoteId());
                 $this->amazonCheckoutSession->clearCheckoutSessionId();
+                $this->updateVersionCookie();
                 return $this->_redirect('checkout/onepage/success');
             } catch (\Exception $e) {
                 $this->exceptionLogger->logException($e);
@@ -103,4 +122,30 @@ class CompleteCheckout extends \Magento\Framework\App\Action\Action
 
         return $this->_redirect('checkout/cart');
     }
+
+    /**
+     * Generate unique version identifier
+     *
+     * @return string
+     */
+    protected function generateValue()
+    {
+        return md5(rand() . time());
+    }
+
+    /**
+     * Update version cookie to clear cart and checkout data on success page
+     *
+     * @return void
+     */
+    protected function updateVersionCookie()
+    {
+        $publicCookieMetadata = $this->cookieMetadataFactory->createPublicCookieMetadata()
+            ->setDuration(Version::COOKIE_PERIOD)
+            ->setPath('/')
+            ->setSecure($this->getRequest()->isSecure())
+            ->setHttpOnly(false);
+        $this->cookieManager->setPublicCookie(Version::COOKIE_NAME, $this->generateValue(), $publicCookieMetadata);
+    }
+
 }
